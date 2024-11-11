b27b1f9026025fce8b5c70b203e3969e70fa5fdc
nikki-pu <nikhila.tiger@gmail.com>
2024-11-11 01:23:33 -0600

    Update script.js

--- script.js
+++ script.js
@@ -28,19 +28,4 @@ window.addEventListener('scroll', checkVisibility);
 checkVisibility();
 
 
-// JavaScript to show the footer on scroll
-// JavaScript to show the footer when scrolling to the bottom
-// JavaScript to show the footer on scroll
-// JavaScript to show the footer only when scrolling all the way to the bottom
-window.addEventListener("scroll", function() {
-    const footer = document.getElementById("scroll-footer");
-    const scrollPosition = window.scrollY + window.innerHeight;
-    const documentHeight = document.documentElement.scrollHeight;
 
-    // Show footer only when reaching the very bottom
-    if (scrollPosition >= documentHeight - 5) { // Allow a small threshold
-        footer.classList.add("show");
-    } else {
-        footer.classList.remove("show");
-    }
-});
